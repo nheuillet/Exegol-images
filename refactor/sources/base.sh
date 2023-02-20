b7a4a4b3b07536fa8cd54d9b45cb86bf8f1c230d
QU35T <qu35t3190@gmail.com>
2023-02-20 12:22:52 +0100

    Remove venv install from pip

--- refactor/sources/base.sh
+++ refactor/sources/base.sh
@@ -47,7 +47,6 @@ function package_base() {
 
   ln -fs /usr/bin/python2.7 /usr/bin/python # Default python is set to 2.7
   install_python-pip              # Pip. Should we set pip2 to default?
-  python -m pip install venv
   python3 -m pip install --upgrade pip
   filesystem
   install_go                      # Golang language
