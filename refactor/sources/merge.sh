c023402c33dc88bdf65813fdeb99e15bda68e2f4
QU35T <qu35t3190@gmail.com>
2023-02-22 10:31:37 +0100

    Fix merge script

--- refactor/sources/merge.sh
+++ refactor/sources/merge.sh
@@ -4,7 +4,7 @@ cp -RT tmp-pipx /root/.local/pipx/ || true
 cp -RT tmp-opt /opt/ || true
 cp -RT tmp-deb /opt/packages || true
 cp -RT tmp-go /root/go/bin/ || true
-cp -RT tmp-history /root/.zsh_history || true
-cp -RT tmp-aliases /opt/.exegol_aliases || true
-cp -RT tmp-commands /.exegol/build_pipeline_tests/all_commands.txt || true
-cp -RT tmp-pipx-symlink /tmp/pipx-symlink || true
+cat tmp-history >> /root/.zsh_history || true
+cat tmp-aliases >> /opt/.exegol_aliases || true
+cat tmp-commands >> /.exegol/build_pipeline_tests/all_commands.txt || true
+cat tmp-pipx-symlink >> /tmp/pipx-symlink || true
